1e0d74409758639066b955187d5a7a62863a22c6
onyeka ossai <onye163@outlook.com>
2025-01-20 03:31:39 -0500

    Add missing dependencies

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'my-vite-app',
+  base: '/my-vite-app',
   plugins: [react()],
   build: {
     rollupOptions: {
