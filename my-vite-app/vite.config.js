2f8371ae454d51b04ea2b8f1cfbf3ccd1a3af4eb
onyeka ossai <onye163@outlook.com>
2025-01-20 04:20:26 -0500

    Remove duplicate json

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-vite-app',
+  // base: '/my-vite-app',
   plugins: [react()],
-  root:'./',
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/react-fontawesome', '@fortawesome/fontawesome-svg-core'],
-    },
-  },
+  // root:'./',
+  // build: {
+  //   rollupOptions: {
+  //     external: ['@fortawesome/react-fontawesome', '@fortawesome/fontawesome-svg-core',
+  //       '@fortawesome/free-solid-svg-icons'
+  //     ],
+  //   },
+  // },
 });
 
